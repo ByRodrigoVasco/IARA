5280927add3a2606801bccfa56d9eb4205206d21
Júlio Lins <juliolinns10@gmail.com>
2026-09-14 20:35:24 -0300

    feat: adicionar maquina de estados e macros relacionados

--- code/src/main.c
+++ code/src/main.c
@@ -1,4 +1,8 @@
 #include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "game_state.h"
+#include "state_machine.h"
 
 #define WINDOW_WIDTH 1024
 #define WINDOW_HEIGHT 576
@@ -18,13 +22,23 @@ int main(void) {
     int text_posX = (WINDOW_WIDTH - textWidth) / 2;
     int text_posY = (WINDOW_HEIGHT - text_in_middle_font_size) / 2;
 
+    initialize_game_state_variables();
+    initialize_hidden_statistic();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-
-            DrawText(text_in_middle, text_posX, text_posY, text_in_middle_font_size, GRAY);
+            DrawText(TextFormat("Dia atual: %d", current_game_state_variables.current_day), 512, 288, text_in_middle_font_size, GRAY);
+            DrawText(TextFormat("Pontos atuais: %d", current_hidden_statistic.current_overall_points), 400, 200, text_in_middle_font_size, GRAY);
         EndDrawing();
+
+        handle_state_machine();
+
+        current_game_state_variables.current_screen++;
+
+        if (current_game_state_variables.current_screen > JUMPING) {
+            current_game_state_variables.current_screen = PRE_MENU;
+        }
     }
 
     CloseWindow();
